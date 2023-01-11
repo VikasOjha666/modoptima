e2847802f9eda7d7b89069b80e6e69ed6fc12690
vikas.ojha <vikas.ojha@datatobiz.com>
2023-01-11 22:41:42 +0530

    Added more dependencies in setup.py and added model usage in README

--- README.md
+++ README.md
@@ -7,11 +7,15 @@ Modoptima is a model optimization library that aims at democratizing model devel
 * Base YOLO V7 (Will be supported in future.)
 
 ### Prerequisites.
-The tiny YOLO V7 optimization is based on sparseml library which requires pytorch==1.8 or 1.9
+The tiny YOLO V7 optimization is based on sparseml library which requires pytorch==1.8 or 1.9. Also it requires deepsparse
+for running the inference hence first these should be installed.
 
 ##### Install pytorch 
 It can be installed by running the below pip command:
 `pip install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html`
+
+##### Install deepsparse
+`pip install deepsparse[yolo]`
 
 
 ### Installation
@@ -34,4 +38,15 @@ You can install modoptima by following below steps:
 Coming soon.
 
 #### USAGE
-Coming soon.
+
+##### Optimizing Model
+
+Modoptima follows two types of optimization i.e Training and non training based the models. 
+YOLO V7 optimization is based on pruning and quantization hence the model is pruned during training to reach a certain sparsity.
+If quantization option is enabled then quantization aware training is done.
+
+For each model supported there is a notebook showing the step by step process to optimize a model and run inference.
+Please check notebooks folder in the repo.
+
+
+
--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 import setuptools
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="modoptima",
@@ -7,13 +16,12 @@ setuptools.setup(
     author="Vikas Kumar Ojha",
     author_email="vikas.ojha894@gmail.com",
     description="ModOptima is a library that democratize deep learning model optimizations to developers. Letting them optimize their models with few lines of code while avoiding the headache of setting up dependencies.",
-    long_description="Description",
+    long_description=long_description,
     packages=['modoptima','modoptima.Train','modoptima.Train.yolov7tiny','modoptima.Train.yolov7tiny.data','modoptima.Train.yolov7tiny.models','modoptima.Train.yolov7tiny.data',
        'modoptima.Train.yolov7tiny.utils','modoptima.Train.yolov7tiny.utils.wandb_logging'],
-    install_requires=['sparseml','deepsparse'],
+    install_requires=['sparseml','numpy','opencv-python','onnx','onnxruntime','tqdm'],
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -22,6 +30,3 @@ setuptools.setup(
     include_package_data=True,
     package_data={'': ['Train/yolov7tiny/data/*.*']},
 )
-
-
-#long_description=open('DESCRIPTION.rst').read()
